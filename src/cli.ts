7b91e40158708e44bd0726ed57f6bd591b9342ab
Vincent Cheung <hello@mynameisvincent.com>
2020-09-15 22:44:09 -0700

    feat(cli): add cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,163 @@
-import * as dgram from 'dgram';
-// import {Setup, SecurityMode} from './broadlink';
+import { Setup, SecurityMode, Discover, Device } from "./broadlink";
+import prompts from "prompts";
+import * as clipboardy from "clipboardy";
 
-// const socket = dgram.createSocket({type: 'udp4', reuseAddr: true});
+(async function () {
+  const action = await prompts([
+    {
+      type: "select",
+      name: "value",
+      message: "What would you like to do?",
+      choices: [
+        { title: "Discover", value: "discover" },
+        { title: "Setup", value: "setup" },
+      ],
+    },
+  ]);
+  switch (action.value) {
+    case "discover":
+      await discover();
+      break;
+    case "setup":
+      await setup();
+      break;
+  }
+})();
 
-// socket.on('listening', function() {
-//   socket.setBroadcast(true);
-//   const packet = Setup("You Have My Sword", "davinchekyrdin", SecurityMode.WPA2);
-//   socket.send(packet, 80, "255.255.255.255", function(err) {
-//     if (err) {
-//       console.error(err);
-//     }
-//     socket.close();
-//   });
-// });
-// socket.bind();
+async function setup() {
+  const response = await prompts([
+    {
+      type: "text",
+      name: "ssid",
+      message: "SSID?",
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "Password?",
+    },
+    {
+      type: "select",
+      name: "wireless",
+      message: "Wireless?",
+      choices: [
+        {
+          title: "None",
+          value: SecurityMode.None,
+        },
+        {
+          title: "WPA1",
+          value: SecurityMode.WPA1,
+        },
+        {
+          title: "WPA2",
+          value: SecurityMode.WPA2,
+        },
+        {
+          title: "WPA1/2",
+          value: SecurityMode.WPA1or2,
+        },
+      ],
+    },
+  ]);
+  try {
+    await Setup(response.ssid, response.password, response.wireless);
+  } catch (e) {
+    console.error(`could not set up device: error=${e}`);
+  }
+}
+
+async function discover() {
+  const sleep = (s: number) => new Promise((resolve) => setTimeout(resolve, s * 1000));
+  const response = await prompts([
+    {
+      type: "number",
+      name: "secs",
+      message: "Timeout (secs)",
+      initial: 2,
+    },
+  ]);
+  console.log("\nSearching...\n");
+  try {
+    let devices: Array<Device> = [];
+    const stream = Discover(response.secs * 1000);
+    stream.listen((device) => devices.push(device));
+    await sleep(response.secs);
+    if (!devices.length) {
+      throw "no devices found";
+    }
+
+    const selectedDevice = await prompts([
+      {
+        type: "select",
+        name: "value",
+        message: "Which Device?",
+        choices: devices.map((d) => {
+          return {
+            value: d,
+            title: `${d.hostport} / ${d.macAddress}`,
+          };
+        }),
+      },
+    ]);
+
+    const d = selectedDevice.value as Device;
+
+    while (true) {
+      const action = await prompts(
+        [
+          {
+            type: "select",
+            name: "value",
+            message: "What to do?",
+            choices: [
+              {
+                title: "Learning Code",
+                value: "learn",
+              },
+              {
+                title: "Read Learned",
+                value: "read",
+              },
+              {
+                title: "Send Code",
+                value: "send",
+              },
+            ],
+          },
+        ],
+        { onCancel: () => process.exit(0) }
+      );
+
+      try {
+        switch (action.value) {
+          case "learn":
+            await d.enterLearning();
+            break;
+          case "read":
+            const data = await d.checkData();
+            const b64 = data.toString("base64");
+            // copy(b64);
+            clipboardy.write(b64);
+            console.log("data:", b64);
+            break;
+          case "send":
+            const resp = await prompts([
+              {
+                type: "text",
+                message: "Data?",
+                name: "value",
+              },
+            ]);
+            const b = Buffer.from(resp.value, "base64");
+            await d.sendData(b);
+            break;
+        }
+      } catch (e) {
+        console.error(`could not perform action; action=${action.value}, error=${e}`);
+      }
+    }
+  } catch (e) {
+    console.error(`could not discover device: error=${e}`);
+  }
+}
